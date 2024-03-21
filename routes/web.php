dfa22330d1a31bcce880c0eff35848735ba62d1b
Mohamed Elshazly <56097354+DevBigBoy@users.noreply.github.com>
2024-03-21 06:45:25 +0200

    Frontend Template Setup Part 1

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\InstructorController;
 */
 
 Route::get('/', function () {
-  return view('welcome');
+  return view('frontend.index')->name('home');
 });
 
 Route::get('/dashboard', function () {
